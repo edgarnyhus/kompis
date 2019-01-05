60fd9ab835daece0b3b2adc5e152a65c8cbefd43
edgar <edgar@getacademy.no>
2019-01-05 17:44:25 +0100

    corrected send mail

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,28 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const axios = require('axios');
-// const urlBuilder = require('build-url');
-// const request = require('request-promise-native');
 const nodemailer = require('nodemailer');
-const postmarkTransport = require('nodemailer-postmark-transport')
+
 
 // Your company name to include in the emails
 // TODO: Change this to your app or company name to customize the email sent.
 const APP_NAME = 'KOMPIS';
+const gmailEmail = "cv.kompis@gmail.com";
+const gmailPassword = "uUamHD8SC9"
+const mailTransport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: gmailEmail,
+    pass: gmailPassword,
+  },
+});
 
-// admin.initializeApp(functions.config().firebase);
+
+// try {
+//     admin.initializeApp(functions.config().firebase)
+// } catch(e) {
+//     console.log('initializeApp failure')
+// }
 admin.initializeApp();
 
 exports.checkAlias = functions.https.onCall((data, context) => {
@@ -25,32 +37,25 @@ exports.checkAlias = functions.https.onCall((data, context) => {
 })
 
 exports.inviteUser = functions.https.onCall((data, context) => {
-    const slug = data.slug
-    const invitee = data.email
-    const mailOptions = {
-        from: `${APP_NAME} <noreply@firebase.com>`,
-        to: invitee,
+    var slug = data.slug;
+    var invitee = data.email;
+
+    //Sends a email to the given user (invitee).
+    var mailOptions = {
+        from : `${APP_NAME} <noreply@firebase.com>`,
+        to : invitee,
     };
-    const postmarkKey = 'AIzaSyCbN1LSb075G2sLa48Fn8d3dexjiYSdHEA' 
-    // const mailTransport = nodemailer.createTransport(postmarkTransport({
-    //     auth: {
-    //         apiKey: postmarkKey
-    //     }
-    // }))
-    const mailTransport = nodemailer.createTransport(
-        `smtps://cv.kompis@gmail.com:6Lfw3x1rM%bTmFJZ@smtp.gmail.com`);
     
-    //Sends a email to the given user.
-    mailOptions.subject = `Du er invitert til ${APP_NAME}!`,
+    // The user subscribed to the newsletter.
+    mailOptions.subject = `Du er invitert til ${APP_NAME}!`;
     mailOptions.html = `<p>Hei ${invitee || ''}!</p><p><strong>${slug}</strong> ønsker å dele sine data med deg på ${'https://cvue-bf9ec.firebaseapp.com/'}.</p><p>Med vennlig hilsen<br>${APP_NAME}</p>`;
     return mailTransport.sendMail(mailOptions)
     .then(() => {
-        return ({ id: doc.id })
+        return { status: 'ok' };
     })
     .catch((error) => {
-        // throw new functions.https.HttpsError('failed to send invite email, ' + error)
-        return ({ error: error })
-    })
+        return { message: 'There was an error while sending the email:', error: error };
+    });
 })
 
 exports.getShortLink = functions.https.onCall((data, context) => {
